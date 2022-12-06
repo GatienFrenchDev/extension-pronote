50500f01f6bff61f0f239b7e16f6ccde5ac55f7f
GatienFrenchDev <gatieng2005@gmail.com>
2022-12-06 11:53:23 +0100

    voila voila

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const url_chrome_store = "https://chrome.google.com/webstore/detail/pronote-%20%20/plkafnalbppdppjehbebdocafmodbbdk"
 const url_firefox_addon = "https://addons.mozilla.org/fr/firefox/addon/pronote/"
+const url_repo_github = "https://github.com/GatienFrenchDev/Pronote-Extension"
 
+document.getElementById("firefox").addEventListener("click", () =>{
+    window.open(url_firefox_addon, "_blank").focus();
+});
 
-document.querySelectorAll('.obtenir_btn').forEach(btn =>{
-    btn.addEventListener("click", () =>{
-        window.open(btn.id == "chrome" ? url_chrome_store:url_firefox_addon, '_blank').focus();
-    });
+document.getElementById("chrome").addEventListener("click", () =>{
+    window.open(url_chrome_store, "_blank").focus();
+});
+
+document.getElementById("github").addEventListener("click", () =>{
+    window.open(url_repo_github, "_blank").focus();
 });
